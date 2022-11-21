4f7c88bf25e201780789c364fb907cf02a8a7caf
Tomas Psota <to.psota@gmail.com>
2022-11-21 23:39:07 +0100

    feat: add files for components

---
+++ src/components/Home.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+const Home = () => <Typography>Home component</Typography>;
+
+export default Home;
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+const NotFound = () => <Typography>NotFound component</Typography>;
+
+export default NotFound;
