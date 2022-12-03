f1713677659dd920b336f7643ae074829e23d899
Tomas Psota <to.psota@gmail.com>
2022-12-03 01:19:02 +0100

    feat: initial work on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,16 @@
 import { Typography } from '@mui/material';
+import { Stack } from '@mui/system';
 
-const Home = () => <Typography>Home component</Typography>;
+import wheel from '../assets/IGT_WOF_wheel.png';
+import './Home.css';
+
+const Home = () => (
+	<Stack sx={{ display: ' flex', alignItems: 'center' }}>
+		<Typography variant="h1" fontWeight="bold">
+			Wheeling fortunate?
+		</Typography>
+		<img src={wheel} alt="game logo" className="rotate" />
+	</Stack>
+);
 
 export default Home;
